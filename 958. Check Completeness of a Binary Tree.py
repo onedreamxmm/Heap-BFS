31f1fd277651baaa52b6d26d2c56fc096a0713e8
onedreamxmm <75698327+onedreamxmm@users.noreply.github.com>
2021-02-02 01:10:22 -0800

    update

--- 958. Check Completeness of a Binary Tree.py
+++ 958. Check Completeness of a Binary Tree.py
@@ -7,26 +7,50 @@ For a complete binary tree,
 there should not be any node after we met an empty one.
 
 Time O(N), Space O(N)
+
 '''
 
-
+from collections import deque
 class TreeNode:
-    def __init__(self, val = 0, left = None, right = None):
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
-    def isCompleteTree(self, root):
+    def isComplete1(self, root: TreeNode) -> bool:
+        queue = deque([root])
+        l = deque([])
+        while queue:
+            node = queue.popleft()
+            l.append(node)
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+        for i in range(len(l)):
+            if not l[i]:
+                for x in range(i+1,len(l)):
+                    if l[x]:
+                        return False
+        return True
+
+    def isComplete2(self, root: TreeNode) -> bool:
         bfs = [root]
         i = 0
         while bfs[i]:
             bfs.append(bfs[i].left)
             bfs.append(bfs[i].right)
             i += 1
-        return not any(bfs[i: ])
+        return not any(bfs[i+1:])
 
 if __name__ == '__main__':
-    root = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, None, TreeNode(7)))
+    root1 = TreeNode(1)
+    root2 = TreeNode(1, TreeNode(2), TreeNode(3))
+    root3 = TreeNode(1, TreeNode(2, TreeNode(3)))
     o = Solution()
-    print(o.isCompleteTree(root))
+    print(o.isComplete1(root1))
+    print(o.isComplete1(root2))
+    print(o.isComplete1(root3))
+    print(o.isComplete2(root1))
+    print(o.isComplete2(root2))
+    print(o.isComplete2(root3))
